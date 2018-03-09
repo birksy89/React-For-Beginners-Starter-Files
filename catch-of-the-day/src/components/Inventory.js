75d9d6f6846f4c2bcc1f916414c5e3f8bd0d3b86
Andrew <andy.birks@gmail.com>
2018-03-09 17:42:35 +0000

    Finished Lesson 6

---
+++ catch-of-the-day/src/components/Inventory.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Inventory extends Component {
+    render() {
+        return (
+           
+           <div className="inventory">
+           Inventory!!!
+           </div>     
+            
+        );
+    }
+}
+
+export default Inventory;
